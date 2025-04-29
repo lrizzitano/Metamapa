e04e8830e940cca63db0db495ee5f245b84c6b66
lrizzitnao <lrizzitano@frba.utn.edu.ar>
2025-04-28 23:35:15 -0300

    constructor de Coleccion

--- src/main/java/ar/edu/utn/frba/dds/Coleccion.java
+++ src/main/java/ar/edu/utn/frba/dds/Coleccion.java
@@ -1,12 +1,20 @@
 package ar.edu.utn.frba.dds;
 
 import java.util.Set;
+import static java.util.Objects.requireNonNull;
 
 public class Coleccion {
   private String titulo;
   private String descripcion;
   private Filtro criterioDePertenencia;
   private Fuente fuente;
+
+  public Coleccion(String titulo, String descripcion, Filtro criterioDePertenencia, Fuente fuente) {
+    this.titulo = requireNonNull(titulo);
+    this.descripcion = requireNonNull(descripcion);
+    this.criterioDePertenencia = requireNonNull(criterioDePertenencia);
+    this.fuente = requireNonNull(fuente);
+  }
 
   public Set<Hecho> hechos(Filtro filtro) {
     return fuente.obtenerHechos(
