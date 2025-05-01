dbe099c9a2a3aa56621898b3e6c5237beb1f1f43
Juanma <jrama@frba.utn.edu.ar>
2025-04-30 22:49:43 -0300

    Añado builder de filtros

--- src/test/java/ar/edu/utn/frba/dds/FiltroBuilderTest.java
+++ src/test/java/ar/edu/utn/frba/dds/FiltroBuilderTest.java
@@ -1,51 +1,56 @@
 package ar.edu.utn.frba.dds;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Predicate;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 class FiltroTest {
   private final FiltroBuilder builder = new FiltroBuilder();
-  private final Predicate<Hecho> filtro1 = hecho -> hecho.getTitulo().contains("incendio");
-  private final Predicate<Hecho> filtro2 = hecho -> hecho.getFechaCarga().equals(LocalDate.now());
-  private final Hecho hecho1 = mock(Hecho.class);
-  private final Hecho hecho2 = mock(Hecho.class);
-  private final Set<Hecho> hechos = new HashSet<>();
+  private final Predicate<Hecho> siempreTrue = hecho -> true;
+  private final Predicate<Hecho> siempreFalse = hecho -> false;
+  private final Hecho hecho = mock(Hecho.class);
 
-  @BeforeEach
-  void setUp() {
-    when(hecho1.getTitulo()).thenReturn("incendio forestal");
-    when(hecho2.getTitulo()).thenReturn("destruccion forestal");
-    when(hecho1.getFechaCarga()).thenReturn(LocalDate.now());
-    when(hecho2.getFechaCarga()).thenReturn(LocalDate.now());
-    hechos.add(hecho1);
-    hechos.add(hecho2);
+
+  @Test
+  void filtroVacioSiempreCumpleParaAnd(){
+    Predicate<Hecho> filtro = builder.obtenerFiltroAnd();
+    Assertions.assertTrue(filtro.test(hecho));
   }
 
   @Test
-  void seFiltraLaLista(){
-    builder.añadirFiltro(filtro1).añadirFiltro(filtro2);
-    Predicate<Hecho> filtro = builder.obtenerFiltro();
-    Assertions.assertEquals(1, hechos.stream().filter(filtro).toList().size());
+  void filtroVacioSiempreCumpleParaOr(){
+    Predicate<Hecho> filtro = builder.obtenerFiltroOr();
+    Assertions.assertTrue(filtro.test(hecho));
   }
 
   @Test
-  void añadirUnFiltroTrueNoAfectaNada(){
-    builder.añadirFiltro(filtro1).añadirFiltro(filtro2).añadirFiltro(hecho -> true);
-    Predicate<Hecho> filtro = builder.obtenerFiltro();
-    Assertions.assertEquals(1, hechos.stream().filter(filtro).toList().size());
+  void trueMasFalseNoCumpleAnd(){
+    builder.añadirFiltro(siempreTrue).añadirFiltro(siempreFalse);
+    Predicate<Hecho>  filtro = builder.obtenerFiltroAnd();
+    Assertions.assertFalse(filtro.test(hecho));
   }
 
   @Test
-  void unFiltroVacioNoAfectaLaLista(){
-    Predicate<Hecho> filtro =  builder.obtenerFiltro();
-    Assertions.assertEquals(2, hechos.stream().filter(filtro).toList().size());
+  void trueMasFalseCumpleOr(){
+    builder.añadirFiltro(siempreTrue).añadirFiltro(siempreFalse);
+    Predicate<Hecho>  filtro = builder.obtenerFiltroOr();
+    Assertions.assertTrue(filtro.test(hecho));
+  }
+
+  @Test
+  void trueMasTrueCumpleAnd(){
+    builder.añadirFiltro(siempreTrue).añadirFiltro(siempreTrue);
+    Predicate<Hecho>  filtro = builder.obtenerFiltroAnd();
+    Assertions.assertTrue(filtro.test(hecho));
+  }
+
+  @Test
+  void falseMasFalseNoCumpleOr(){
+    builder.añadirFiltro(siempreFalse).añadirFiltro(siempreFalse);
+    Predicate<Hecho>  filtro = builder.obtenerFiltroOr();
+    Assertions.assertFalse(filtro.test(hecho));
   }
 }
