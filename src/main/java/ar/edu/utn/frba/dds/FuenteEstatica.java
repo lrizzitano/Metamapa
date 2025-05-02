e6336bc9e088290f497cd04196ed245b862b5334
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-05-02 09:49:08 -0300

    Agrego comportamiento al administrador y la clase fuente estatica

---
+++ src/main/java/ar/edu/utn/frba/dds/FuenteEstatica.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.frba.dds;
+
+import java.util.Set;
+
+public class FuenteEstatica{
+
+  private final String pathArchivo;
+
+  public FuenteEstatica(String pathArchivo) {
+    this.pathArchivo = pathArchivo;
+  }
+
+  //@Override
+  //public Set<Hecho> obtenerHechos(Filtro criterio) {
+    // to do
+  //}
+}
