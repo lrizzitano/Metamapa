7898f470338146c1b170a220c4241db146876abf
Lautaro <lautaromelchiori01@gmail.com>
2025-05-02 21:17:43 -0300

    cambio campo ubicacion por campos latitud y longitud

--- src/main/java/ar/edu/utn/frba/dds/Hecho.java
+++ src/main/java/ar/edu/utn/frba/dds/Hecho.java
@@ -7,31 +7,33 @@ public class Hecho{
   private final String titulo;
   private final String descripcion;
   private final String categoria;
-  private final String ubicacion;
+  private final Double latitud;
+  private final Double longitud;
   private final LocalDate fechaCarga;
   private final LocalDate fechaAcontecimiento;
   private final Origen origen;
   //private TipoHecho tipo; MULTIMEDIA o CONTRIBUYENTE
 
-  public Hecho(String titulo,String descripcion,String categoria,String ubicacion,LocalDate fechaCarga,
+  public Hecho(String titulo,String descripcion,String categoria, Double latitud, Double longitud,LocalDate fechaCarga,
                LocalDate fechaAcontecimiento,Origen origen){
 
-    if(titulo==null){throw new HechoInvalidoException("No se asigno ningun Titulo");}
-    if(descripcion==null){throw new HechoInvalidoException("No se asigno ninguna Descripcion");}
-    if(categoria==null){throw new HechoInvalidoException("No se asigno ninguna Categoria");}
-    if(ubicacion==null){throw new HechoInvalidoException("No se asigno ninguna Ubicacion");}
-    if(fechaCarga==null){throw new HechoInvalidoException("No se asigno ninguna Fecha de Carga");}
-    if(fechaAcontecimiento==null){throw new HechoInvalidoException("No se asigno ninguna Fecha de Acontecimiento");}
+    if(titulo==null){throw new HechoInvalidoException("No se asigno ningun titulo");}
+    if(descripcion==null){throw new HechoInvalidoException("No se asigno ninguna descripcion");}
+    if(categoria==null){throw new HechoInvalidoException("No se asigno ninguna categoria");}
+    if(latitud==null){throw new HechoInvalidoException("No se asigno ninguna latitud");}
+    if(longitud==null){throw new HechoInvalidoException("No se asigno ninguna longitud");}
+    if(fechaCarga==null){throw new HechoInvalidoException("No se asigno ninguna fecha de carga");}
+    if(fechaAcontecimiento==null){throw new HechoInvalidoException("No se asigno ninguna fecha de acontecimiento");}
     if(origen==null){throw new HechoInvalidoException("No se asigno ningun origen");}
 
     this.titulo = titulo;
     this.descripcion = descripcion;
     this.categoria = categoria;
-    this.ubicacion = ubicacion;
+    this.latitud = latitud;
+    this.longitud = longitud;
     this.fechaCarga = fechaCarga;
     this.fechaAcontecimiento = fechaAcontecimiento;
     this.origen = origen;
-
   }
 
   public String getTitulo() {
