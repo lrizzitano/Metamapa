ea33ba7dd8fc74118dccafa7bd2379245886fa51
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-05-08 13:31:32 -0300

    Cambio nombres

--- src/main/java/ar/edu/utn/frba/dds/Filtro.java
+++ src/main/java/ar/edu/utn/frba/dds/Filtro.java
@@ -8,29 +8,29 @@ public enum Filtro {
   TITULO(valor -> contiene(Hecho::titulo, valor)),
   DESCRIPCION(valor -> contiene(Hecho::descripcion, valor)),
   CATEGORIA(valor -> contiene(Hecho::categoria, valor)),
-  LATITUD_MAYOR(valor -> compararDouble(Hecho::latitud, valor, Comparar.MAYOR)),
-  LATITUD_MENOR(valor -> compararDouble(Hecho::latitud, valor, Comparar.MENOR)),
-  LONGITUD_MAYOR(valor -> compararDouble(Hecho::longitud, valor, Comparar.MAYOR)),
-  LONGITUD_MENOR(valor -> compararDouble(Hecho::longitud, valor, Comparar.MENOR)),
-  FECHA_CARGA_ANTES(valor -> compararFecha(Hecho::fechaCarga, valor, Comparar.MENOR)),
-  FECHA_CARGA_DESPUES(valor -> compararFecha(Hecho::fechaCarga, valor, Comparar.MAYOR)),
-  FECHA_HECHO_ANTES(valor -> compararFecha(Hecho::fechaAcontecimiento, valor, Comparar.MENOR)),
-  FECHA_HECHO_DESPUES(valor -> compararFecha(Hecho::fechaAcontecimiento, valor, Comparar.MAYOR));
+  LATITUD_MAYOR(valor -> compararDouble(Hecho::latitud, valor, TipoComparacion.MAYOR)),
+  LATITUD_MENOR(valor -> compararDouble(Hecho::latitud, valor, TipoComparacion.MENOR)),
+  LONGITUD_MAYOR(valor -> compararDouble(Hecho::longitud, valor, TipoComparacion.MAYOR)),
+  LONGITUD_MENOR(valor -> compararDouble(Hecho::longitud, valor, TipoComparacion.MENOR)),
+  FECHA_CARGA_ANTES(valor -> compararFecha(Hecho::fechaCarga, valor, TipoComparacion.MENOR)),
+  FECHA_CARGA_DESPUES(valor -> compararFecha(Hecho::fechaCarga, valor, TipoComparacion.MAYOR)),
+  FECHA_HECHO_ANTES(valor -> compararFecha(Hecho::fechaAcontecimiento, valor, TipoComparacion.MENOR)),
+  FECHA_HECHO_DESPUES(valor -> compararFecha(Hecho::fechaAcontecimiento, valor, TipoComparacion.MAYOR));
   //ORIGEN_ES(valor -> contiene(Hecho::origen, valor)),
   //ORIGEN_NO_ES(valor -> hecho -> !hecho.origen().toLowerCase().contains(valor.toLowerCase()));
 
-  private final Function<String, Predicate<Hecho>> constructor;
+  private final Function<String, Predicate<Hecho>> funcionCreadora;
 
-  Filtro(Function<String, Predicate<Hecho>> constructor) {
-    this.constructor = constructor;
+  Filtro(Function<String, Predicate<Hecho>> funcionCreadora) {
+    this.funcionCreadora = funcionCreadora;
   }
 
   public Predicate<Hecho> crearFiltro(String valor) {
-    return constructor.apply(valor);
+    return funcionCreadora.apply(valor);
   }
 
   //Auxiliares
-  private enum Comparar { MAYOR, MENOR }
+  private enum TipoComparacion { MAYOR, MENOR }
 
   private static Predicate<Hecho> contiene(Function<Hecho, String> getter, String valor) {
     String lowered = valor.toLowerCase();
@@ -38,15 +38,16 @@ public enum Filtro {
   }
 
   private static Predicate<Hecho> compararDouble(Function<Hecho, Double> getter, String valor,
-                                                 Comparar cmp) {
-    return hecho -> cmp == Comparar.MAYOR
+                                                 TipoComparacion tipoComparacion) {
+    return hecho -> tipoComparacion == TipoComparacion.MAYOR
         ? getter.apply(hecho) >= Double.parseDouble(valor)
         : getter.apply(hecho) <= Double.parseDouble(valor);
   }
 
   private static Predicate<Hecho> compararFecha(Function<Hecho, LocalDate> getter, String valor,
-                                                Comparar cmp) {
-    return hecho -> cmp == Comparar.MAYOR
+                                                TipoComparacion tipoComparacion) {
+    return hecho -> getter.apply(hecho).equals(LocalDate.parse(valor)) ||
+        tipoComparacion == TipoComparacion.MAYOR
         ? getter.apply(hecho).isAfter(LocalDate.parse(valor))
         : getter.apply(hecho).isBefore(LocalDate.parse(valor));
   }
