e8bf81f63683984d3a8229752057d115fe8c309f
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-05-20 13:54:14 -0300

    Elimino responsabilidades innecesarias del administrador y sus test

--- src/main/java/ar/edu/utn/frba/dds/Administrador.java
+++ src/main/java/ar/edu/utn/frba/dds/Administrador.java
@@ -2,34 +2,10 @@ package ar.edu.utn.frba.dds;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Set;
-import java.util.function.Predicate;
-
 public class Administrador extends Usuario {
 
   public Administrador(String nombre, String apellido, int edad) {
     super(requireNonNull(nombre), requireNonNull(apellido), edad);
   }
 
-  public Coleccion crearColeccion(String titulo, String descripcion, Predicate<Hecho>
-      criterioDePertenencia, Fuente fuente) {
-
-    return new Coleccion(titulo, descripcion, criterioDePertenencia, fuente);
-  }
-
-  public Set<Solicitud> verSolicitudes() {
-    return Solicitudes.instance().getPendientes();
-  }
-
-  public FuenteEstatica crearFuenteEstatica(String pathArchivo) {
-    return new FuenteEstatica(pathArchivo);
-  }
-
-  public void aceptarSolicitud(Solicitud solicitud) {
-    solicitud.aceptar(this);
-  }
-
-  public void rechazarSolicitud(Solicitud solicitud) {
-    solicitud.rechazar(this);
-  }
 }
