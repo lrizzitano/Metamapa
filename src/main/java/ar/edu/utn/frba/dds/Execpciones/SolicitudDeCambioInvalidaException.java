bf63a5b22fd9534b2d6d9bfbcc03d12e401178f3
PGabarini <pagabarini@frba.utn.edu.ar>
2025-05-30 17:34:05 -0300

    Agrego funcionalidad al Usuario para que Cree Hecho| discutible

---
+++ src/main/java/ar/edu/utn/frba/dds/Execpciones/SolicitudDeCambioInvalidaException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.Execpciones;
+
+public class SolicitudDeCambioInvalidaException extends RuntimeException {
+  public SolicitudDeCambioInvalidaException(String message) {
+    super(message);
+  }
+}
--- src/main/java/ar/edu/utn/frba/dds/Usuarios/Usuario.java
+++ src/main/java/ar/edu/utn/frba/dds/Usuarios/Usuario.java
@@ -1,6 +1,10 @@
 package ar.edu.utn.frba.dds.Usuarios;
 
 
+import ar.edu.utn.frba.dds.Fuentes.FuenteDinamica;
+import ar.edu.utn.frba.dds.Hechos.Hecho;
+import ar.edu.utn.frba.dds.Solicitudes.SolicitudDeCambio;
+
 public class Usuario {
 
   private String nombre;
@@ -28,6 +32,14 @@ public class Usuario {
 
   public boolean esContribuyente() {
     return esContribuyente;
+  }
+
+  //Podria ir en la fuente pero CREO QUE HAY razones para que este aca
+  public void agregarHecho(Hecho hecho)
+  {
+    //hecho.setContribuyente(this); ESTO DEBE FUNCIONAR, O SE ASUME QUE SE LLAMA CON UN HECHO CON ESE USUARIO
+    FuenteDinamica.instance().agregarHecho(hecho);
+    this.esContribuyente = true;
   }
 
 }
