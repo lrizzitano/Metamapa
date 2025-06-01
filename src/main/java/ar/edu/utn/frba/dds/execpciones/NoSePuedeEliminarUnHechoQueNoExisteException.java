fc2910820343a4726fdfe3bd4e0246a60058505e
PGabarini <pagabarini@frba.utn.edu.ar>
2025-05-31 23:42:40 -0300

    Agrego excepcion al intetar eliminar hecho que no existe

--- src/main/java/ar/edu/utn/frba/dds/execpciones/NoSePuedeEliminarUnHechoQueNoExisteException.java
+++ src/main/java/ar/edu/utn/frba/dds/execpciones/NoSePuedeEliminarUnHechoQueNoExisteException.java
@@ -1,7 +1,7 @@
 package ar.edu.utn.frba.dds.execpciones;
 
-public class NoSePuedeEliminarUnHechoQueNoExiste extends RuntimeException {
-  public NoSePuedeEliminarUnHechoQueNoExiste(String message) {
-    super("No puedes eliminar un hecho que no existe en la fuente dinamica" + message);
+public class NoSePuedeEliminarUnHechoQueNoExisteException extends RuntimeException {
+  public NoSePuedeEliminarUnHechoQueNoExisteException() {
+    super("No puedes eliminar un hecho que no existe en la fuente dinamica");
   }
 }
