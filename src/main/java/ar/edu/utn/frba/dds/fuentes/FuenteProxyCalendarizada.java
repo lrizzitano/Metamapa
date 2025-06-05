78f78e0f187a02f469685533053e2f0803fba842
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-06-05 12:39:41 -0300

    Ignoren

--- src/main/java/ar/edu/utn/frba/dds/fuentes/FuenteProxyCalendarizada.java
+++ src/main/java/ar/edu/utn/frba/dds/fuentes/FuenteProxyCalendarizada.java
@@ -10,13 +10,9 @@ import java.util.stream.Collectors;
 public abstract class FuenteProxyCalendarizada implements Fuente {
   private final Set<Hecho> hechos = ConcurrentHashMap.newKeySet();
 
-  public FuenteProxyCalendarizada(Duration frecuencia) {
-    this.frecuencia = frecuencia;
+  public FuenteProxyCalendarizada() {
   }
 
-  public Duration getFrecuencia() {
-    return frecuencia;
-  }
 
   public void actualizarHechos(Instant ultimaLlamada) {
     hechos.addAll(getNewHechos(ultimaLlamada));
