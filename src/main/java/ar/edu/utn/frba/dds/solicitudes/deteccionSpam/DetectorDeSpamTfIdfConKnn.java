be1513fd38619ee48428f8fc5d638bcdf971b064
Lautaro <lautaromelchiori01@gmail.com>
2025-06-05 16:35:47 -0300

    detector de spam con tf-idf + knn | Funcionando

--- src/main/java/ar/edu/utn/frba/dds/solicitudes/deteccionSpam/DetectorDeSpamTfIdfConKnn.java
+++ src/main/java/ar/edu/utn/frba/dds/solicitudes/deteccionSpam/DetectorDeSpamTfIdfConKnn.java
@@ -1,11 +1,23 @@
 package ar.edu.utn.frba.dds.solicitudes.deteccionSpam;
 
+import java.util.List;
+
 public class DetectorDeSpamTfIdfConKnn implements DetectorDeSpam {
-  Vectorizador vectorizador;
-  ClasificadorKNN clasificador;
+  VectorizadorTfIdf vectorizador;
+  ClasificadorKnn clasificador;
+
+  DetectorDeSpamTfIdfConKnn(List<Solicitud> corpusDeEjemplo, int cantidadDeVecinos) {
+    List<String> justificaciones = corpusDeEjemplo.stream().map(s -> s.justificacion).toList();
+    this.vectorizador = new VectorizadorTfIdf(justificaciones);
+
+    List<VectorEtiquetado> vectoresEtiquetados = corpusDeEjemplo.stream()
+        .map(s -> new VectorEtiquetado(vectorizador.vectorizar(s.justificacion), s.etiqueta)).toList();
+
+    this.clasificador = new ClasificadorKnn(vectoresEtiquetados, cantidadDeVecinos);
+  }
 
   @Override
   public boolean esSpam(String justificacion) {
-    return true;
+    return clasificador.esDeCategoria(Categoria.SPAM, vectorizador.vectorizar(justificacion));
   }
 }
