51880c4a71536e837bb142476ddf52c7d95d2a0e
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-06-05 14:40:57 -0300

    agrego interfaz de solicitudes de eliminacion

---
+++ src/main/java/ar/edu/utn/frba/dds/solicitudes/SolicitudDeEliminacionRepository.java
@@ -0,0 +1,17 @@
+package ar.edu.utn.frba.dds.solicitudes;
+
+import ar.edu.utn.frba.dds.hechos.Hecho;
+import java.util.Map;
+import java.util.Set;
+
+public interface SolicitudDeEliminacionRepository {
+    void nuevaSolicitud(SolicitudDeEliminacion solicitud);
+    void aceptarSolicitud(SolicitudDeEliminacion solicitud);
+    void rechazarSolicitud(SolicitudDeEliminacion solicitud);
+    Set<SolicitudDeEliminacion> getPendientes();
+    Set<SolicitudDeEliminacion> getAceptadas();
+    Map<Hecho, Integer> getRechazadas();
+    Integer getRechazos(Hecho hecho);
+    boolean estaEliminado(Hecho hecho);
+    Set<Hecho> hechosEliminados();
+}
