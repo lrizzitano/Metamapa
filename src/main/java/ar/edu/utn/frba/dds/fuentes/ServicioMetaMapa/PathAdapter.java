fa97f3be26d4a463ce483216234f3aa2a15ba238
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-06-06 10:21:59 -0300

    Demuestro opcionalidad del path en el adapter

--- src/main/java/ar/edu/utn/frba/dds/fuentes/ServicioMetaMapa/PathAdapter.java
+++ src/main/java/ar/edu/utn/frba/dds/fuentes/ServicioMetaMapa/PathAdapter.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 public class PathAdapter extends TypeAdapter<Path> {
   @Override
   public void write(JsonWriter out, Path value) throws IOException {
-    out.value(value.toString()); // como string tipo "D:/archivos/foo.txt"
+    out.value(value == null ? null : value.toString()); // como string tipo "D:/archivos/foo.txt"
   }
 
   @Override
