3f9bc865bcb08ae5f258374a3f342f273e3de433
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-06-30 15:17:09 -0300

    Implemento algoritmo de multiples menciones

--- src/main/java/ar/edu/utn/frba/dds/hechos/consenso/CriterioMultiplesMenciones.java
+++ src/main/java/ar/edu/utn/frba/dds/hechos/consenso/CriterioMultiplesMenciones.java
@@ -1,7 +1,12 @@
 package ar.edu.utn.frba.dds.hechos.consenso;
 
+import ar.edu.utn.frba.dds.filtros.Filtro;
+import ar.edu.utn.frba.dds.filtros.NullFiltro;
 import ar.edu.utn.frba.dds.fuentes.Fuente;
-import java.util.Set;
+import ar.edu.utn.frba.dds.hechos.Hecho;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CriterioMultiplesMenciones extends CriterioConsenso{
 
@@ -9,7 +14,42 @@ public class CriterioMultiplesMenciones extends CriterioConsenso{
   public void actualizar(){
     Set<Fuente> fuentes = this.fuentesRepository.getFuentes();
 
-    //Algortirmo para actualizar los hechos consensuados
+    Filtro nullFiltro = new NullFiltro();
+
+    Map<String, List<Hecho>> hechosConsensuadosNuevos = new HashMap<>(); // <nombre, hechos>
+
+    for (Fuente f : fuentes) {
+      Set<Hecho> hechos = f.obtenerHechos(nullFiltro);
+
+      for (Hecho h : hechos) {
+        // Si no existe el hecho en el hashmap lo agrega
+        // Si ya aparecio por otra fuente, le suma uno a las apariciones
+        // aguante la tabla de hash
+          hechosConsensuadosNuevos
+              .compute(h.titulo(),
+                  (titulo, hechosEsteTitulo) -> {
+                    if (hechosEsteTitulo == null) {
+                      List<Hecho> nuevaLista = new ArrayList<>();
+                      nuevaLista.add(h);
+                      return nuevaLista;
+                    } else {
+                      hechosEsteTitulo.add(h);
+                      return hechosEsteTitulo;
+                    }
+                  });
+      }
+    }
+
+    this.hechosConsensuados = hechosConsensuadosNuevos.values().stream()
+        .filter(hechos -> hechos.size() > 1 && !this.compartenAtributos(hechos))
+        .map(lista -> lista.get(0))
+        .collect(Collectors.toSet());
   }
 
+  private boolean compartenAtributos(List<Hecho> hechos) {
+    return hechos.stream()
+        .map(Hecho::titulo)
+        .distinct()
+        .count() <= 1;
+  }
 }
