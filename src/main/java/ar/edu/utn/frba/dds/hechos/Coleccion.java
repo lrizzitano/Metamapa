9a51bf3d0705949bbffcc1f03b680d79dda1622a
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-06-30 13:14:06 -0300

    Añado criterio de concenso

--- src/main/java/ar/edu/utn/frba/dds/hechos/Coleccion.java
+++ src/main/java/ar/edu/utn/frba/dds/hechos/Coleccion.java
@@ -6,11 +6,15 @@ import ar.edu.utn.frba.dds.filtros.Filtro;
 import ar.edu.utn.frba.dds.filtros.FiltroCompuesto;
 import ar.edu.utn.frba.dds.filtros.FiltroEliminados;
 import ar.edu.utn.frba.dds.fuentes.Fuente;
+import ar.edu.utn.frba.dds.hechos.consenso.CriterioConsenso;
 import ar.edu.utn.frba.dds.solicitudes.SolicitudDeEliminacionRepository;
 import ar.edu.utn.frba.dds.solicitudes.SolicitudesDeEliminacion;
 import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Coleccion {
   private final String titulo;
@@ -18,13 +22,16 @@ public class Coleccion {
   private final FiltroCompuesto criterioDePertenencia;
   private final Fuente fuente;
   private final String id = UUID.randomUUID().toString();
-  private SolicitudDeEliminacionRepository solicitudes;
+  private final CriterioConsenso criterioConsenso;
+  private final SolicitudDeEliminacionRepository solicitudes;
 
   public Coleccion(String titulo, String descripcion,
                    Filtro criterioDePertenencia,
-                   Fuente fuente, SolicitudDeEliminacionRepository solicitudes) {
+                   Fuente fuente, CriterioConsenso criterioConsenso,
+                   SolicitudDeEliminacionRepository solicitudes) {
     this.titulo = requireNonNull(titulo);
     this.descripcion = requireNonNull(descripcion);
+    this.criterioConsenso = requireNonNull(criterioConsenso);
     requireNonNull(criterioDePertenencia);
     this.criterioDePertenencia =
         new FiltroCompuesto(Collections.singletonList(criterioDePertenencia));
@@ -45,10 +52,29 @@ public class Coleccion {
   }
 
   public Set<Hecho> hechos(Filtro filtro) {
-    return fuente.obtenerHechos(criterioDePertenencia.and(filtro).and(this.condicionNoEliminado()));
+    return this.streamFiltradaBase(filtro)
+        .collect(Collectors.toSet());
   }
 
-  private Filtro condicionNoEliminado() {
-    return new FiltroEliminados(solicitudes);
+  public Set<Hecho> hechosConsensuados(Filtro filtro) {
+    return this.streamFiltradaBase(filtro)
+        .filter(this.condicionConsenso())
+        .collect(Collectors.toSet());
+  }
+
+  private Stream<Hecho> streamFiltradaBase(Filtro filtro) {
+    return fuente.obtenerHechos(criterioDePertenencia.and(filtro)).stream()
+        .filter(this.condicionNoEliminado());
+  }
+
+  private Predicate<Hecho> condicionNoEliminado() {
+    return h -> {
+      Set<Hecho> eliminados = solicitudes.hechosEliminados();
+      return !eliminados.contains(h);
+    };
+  }
+
+  private Predicate<Hecho> condicionConsenso() {
+    return hecho-> criterioConsenso.getHechosConsensuados().contains(hecho);
   }
 }
