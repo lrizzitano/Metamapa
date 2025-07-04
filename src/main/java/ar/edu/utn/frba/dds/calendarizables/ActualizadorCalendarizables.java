db11dd056526a39f7aa84af46b8fbb12efab5d85
Lautaro <lautaromelchiori01@gmail.com>
2025-07-04 09:47:23 -0300

    agrego printf en main para comprobar que esta corriendo

--- src/main/java/ar/edu/utn/frba/dds/calendarizables/ActualizadorCalendarizables.java
+++ src/main/java/ar/edu/utn/frba/dds/calendarizables/ActualizadorCalendarizables.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frba.dds.calendarizables;
 
 public class ActualizadorCalendarizables {
   public static void main(String[] args) {
+    System.out.println("Voy a actualizar!");
     CalendarizablesRepository repo = CalendarizablesRepository.instance();
     repo.getPendientesDeActualizar().forEach(c -> {
       try {
