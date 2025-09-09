e0975b90112aabfa074c57cfafc2185039b81a9f
Lautaro <lautaromelchiori01@gmail.com>
2025-09-09 15:34:41 -0300

    corrijo typo en repo de colecciones

--- src/main/java/ar/edu/utn/frba/dds/repositorios/RepoColecciones.java
+++ src/main/java/ar/edu/utn/frba/dds/repositorios/RepoColecciones.java
@@ -1,11 +1,11 @@
 package ar.edu.utn.frba.dds.repositorios;
 
-import ar.edu.utn.frba.dds.hechos.Hecho;
+import ar.edu.utn.frba.dds.hechos.Coleccion;
 
-public class RepoColecciones extends RepoGenerico<Hecho>{
+public class RepoColecciones extends RepoGenerico<Coleccion>{
 
   public RepoColecciones() {
-    super(Hecho.class);
+    super(Coleccion.class);
   }
 
 }
