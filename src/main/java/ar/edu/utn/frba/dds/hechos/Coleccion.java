7f36e40a2e04c9ce8f0b3a7e91046d450a78a73d
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-09-11 18:11:22 -0300

    Atributo id duplicado en Coleccion

--- src/main/java/ar/edu/utn/frba/dds/hechos/Coleccion.java
+++ src/main/java/ar/edu/utn/frba/dds/hechos/Coleccion.java
@@ -31,7 +31,7 @@ public class Coleccion{
   @Id
   @Column(name = "coleccion_id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  public Long idd;
+  public Long id;
 
   @Column(name = "titulo",nullable = false)
   private String titulo;
@@ -48,9 +48,6 @@ public class Coleccion{
   @ManyToOne
   @JoinColumn(name = "fuente",nullable = false)
   private Fuente fuente;
-
-  @Transient
-  private final String id = UUID.randomUUID().toString();
 
   @ManyToOne
   @JoinColumn(name = "criterioConsenso",nullable = false)
@@ -81,7 +78,7 @@ public class Coleccion{
     return descripcion;
   }
 
-  public String getId() {
+  public Long getId() {
     return id;
   }
 
