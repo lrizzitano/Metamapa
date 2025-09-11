61293b38ecee9a0be1df9034846eaae4f7dfe887
Lautaro <lautaromelchiori01@gmail.com>
2025-09-11 15:27:31 -0300

    sumo publicador de estadisticas csv

--- src/main/java/ar/edu/utn/frba/dds/estadisticas/publicadorDeResultados/PublicadorDeResultadosCSV.java
+++ src/main/java/ar/edu/utn/frba/dds/estadisticas/publicadorDeResultados/PublicadorDeResultadosCSV.java
@@ -1,15 +1,41 @@
 package ar.edu.utn.frba.dds.estadisticas.publicadorDeResultados;
 
 import ar.edu.utn.frba.dds.estadisticas.resultadoEstadistico.ResultadoEstadistico;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class PublicadorDeResultadosCSV implements PublicadorDeResultados {
+  public String nombreArchivo;
 
-  PublicadorDeResultadosCSV() {}
+  public PublicadorDeResultadosCSV(String nombreArchivo) {
+    this.nombreArchivo = nombreArchivo;
+  }
 
   @Override
-  public void comunicarResultados(List<ResultadoEstadistico> resultado) {
+  public void comunicarResultados(List<ResultadoEstadistico> resultados) {
+    if (resultados == null || resultados.isEmpty()) return;
 
+    try (FileWriter writer = new FileWriter(nombreArchivo, true)) {
+      File archivo = new File(nombreArchivo);
+      if (!archivo.exists() || archivo.length() == 0) {
+        // encabezado a partir del primer objeto
+        Map<String, String> headers = resultados.get(0).infoExportable();
+        writer.append(String.join(",", headers.keySet()));
+        writer.append("\n");
+      }
+
+      // filas
+      for (ResultadoEstadistico res : resultados) {
+        Map<String, String> fila = res.infoExportable();
+        writer.append(String.join(",", fila.values()));
+        writer.append("\n");
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
