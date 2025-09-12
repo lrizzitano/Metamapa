39f4cbbabe21f2fdb2bb3512fa2b8abe1bb8efe8
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-09-12 02:39:03 -0300

    Habia una manera mas prolija de solucionarlo, el problema es solo con la persistencia, no con la definicion de las entidades en sí, no estoy seguro porque fallaba antes

--- src/test/java/ar/edu/utn/frba/dds/estadisticas/EstadisticoTest.java
+++ src/test/java/ar/edu/utn/frba/dds/estadisticas/EstadisticoTest.java
@@ -5,35 +5,21 @@ import ar.edu.utn.frba.dds.estadisticas.resultadoEstadistico.ResultadoEstudioCat
 import ar.edu.utn.frba.dds.estadisticas.resultadoEstadistico.ResultadoEstudioColeccion;
 import ar.edu.utn.frba.dds.filtros.Filtro;
 import ar.edu.utn.frba.dds.filtros.NullFiltro;
-import ar.edu.utn.frba.dds.fuentes.Fuente;
 import ar.edu.utn.frba.dds.fuentes.FuenteDinamica;
 import ar.edu.utn.frba.dds.hechos.Coleccion;
 import ar.edu.utn.frba.dds.hechos.Provincia;
 import ar.edu.utn.frba.dds.hechos.consenso.Consenso;
 import ar.edu.utn.frba.dds.hechos.consenso.ConsensoNull;
 import ar.edu.utn.frba.dds.repositorios.FuentesRepositoryJPA;
-import ar.edu.utn.frba.dds.repositorios.HechosFuenteDinamicaJPA;
 import ar.edu.utn.frba.dds.repositorios.RepoColecciones;
-import ar.edu.utn.frba.dds.repositorios.RepoUsuarios;
-import ar.edu.utn.frba.dds.repositorios.solicitudes.SolicitudDeEliminacionRepository;
 import ar.edu.utn.frba.dds.repositorios.solicitudes.SolicitudesDeEliminacionJPA;
-import ar.edu.utn.frba.dds.solicitudes.SolicitudDeEliminacion;
-import ar.edu.utn.frba.dds.solicitudes.deteccionSpam.NullDetector;
-import ar.edu.utn.frba.dds.usuarios.Administrador;
 import io.github.flbulgarelli.jpa.extras.test.SimplePersistenceTest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
-import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class EstadisticoTest implements SimplePersistenceTest {
@@ -59,51 +45,46 @@ public class EstadisticoTest implements SimplePersistenceTest {
   ResultadoEstudioCategoria estudioCategoria;
 
   @BeforeEach
-   void setUp() {
+  void setUp() {
 
+    fecha = LocalDateTime.now();
 
-    hpc1 = new HechosPorProvincia(Provincia.LA_PAMPA,(long)3);
-     hpc2 = new HechosPorProvincia(Provincia.LA_RIOJA,(long) 2);
+    estadistico = new Estadistico();
+
+    hpc1 = new HechosPorProvincia(Provincia.LA_PAMPA, (long) 3);
+    hpc2 = new HechosPorProvincia(Provincia.LA_RIOJA, (long) 2);
 
     hechosXprovincia = List.of(hpc1, hpc2);
     consensoNull = new ConsensoNull();
-     filtroNulo = new NullFiltro();
+    filtroNulo = new NullFiltro();
+
+    estudioCategoria =
+        new ResultadoEstudioCategoria(fecha, "Incendio", 5, 12, hechosXprovincia);
+
+    coleccionAux = new Coleccion("prueb", "Una descripcion", filtroNulo,
+        fuenteDinamica, consensoNull, new SolicitudesDeEliminacionJPA());
+
+    estudioColeccion =
+        new ResultadoEstudioColeccion(fecha, coleccionAux, (long) 5, hechosXprovincia);
   }
 
   @Test
   public void provinciaConMasHechosDeUnaColleccion() {
 
-    estadistico = new Estadistico();
-
     repoFuentes.agregarFuente(fuenteDinamica);
 
-
-    coleccionAux = new Coleccion("prueb","Una descripcion",filtroNulo,
-        fuenteDinamica,consensoNull,new SolicitudesDeEliminacionJPA());
     repoColecciones.save(coleccionAux);
-
-    fecha = LocalDateTime.now();
-
-    estudioColeccion =
-        new ResultadoEstudioColeccion(fecha,coleccionAux,(long)5,hechosXprovincia);
 
     entityManager().persist(estudioColeccion);
 
-    String provincia = estadistico.provinciaConMayorCantidadDeHechosReportadosDeColeccion(coleccionAux,fecha);
+    String provincia = estadistico.provinciaConMayorCantidadDeHechosReportadosDeColeccion(coleccionAux, fecha);
 
-    Assertions.assertEquals("LA_PAMPA",provincia);
+    Assertions.assertEquals("LA_PAMPA", provincia);
     System.out.println(provincia);
   }
 
   @Test
   public void categoriaConMasHechosReportados() {
-
-    fecha = LocalDateTime.now();
-
-    estudioCategoria =
-        new ResultadoEstudioCategoria(fecha,"Incendio",5, 12 ,hechosXprovincia);
-
-    estadistico = new Estadistico();
 
     entityManager().persist(estudioCategoria);
 
@@ -111,42 +92,28 @@ public class EstadisticoTest implements SimplePersistenceTest {
 
     System.out.println(categoria);
 
-    Assertions.assertEquals("Incendio",categoria);
+    Assertions.assertEquals("Incendio", categoria);
 
   }
 
   @Test
-  public void horaPicoDeReporteDeUnaCategoria(){
-
-    fecha = LocalDateTime.now();
-
-    estudioCategoria =
-        new ResultadoEstudioCategoria(fecha,"Incendio",5, 12 ,hechosXprovincia);
-
-    estadistico = new Estadistico();
+  public void horaPicoDeReporteDeUnaCategoria() {
 
     entityManager().persist(estudioCategoria);
 
-    LocalTime horaPico = estadistico.horaPicoDeReporteDeUnaCategoria("Incendio",fecha);
+    LocalTime horaPico = estadistico.horaPicoDeReporteDeUnaCategoria("Incendio", fecha);
 
-    Assertions.assertEquals("00:12",horaPico.toString());
+    Assertions.assertEquals("00:12", horaPico.toString());
   }
 
   @Test
-  public void provinciaConMasHechosReportadosDeUnaCategoria(){
-
-    fecha = LocalDateTime.now();
-
-    estudioCategoria =
-        new ResultadoEstudioCategoria(fecha,"Incendio",5, 12 ,hechosXprovincia);
-
-    estadistico = new Estadistico();
+  public void provinciaConMasHechosReportadosDeUnaCategoria() {
 
     entityManager().persist(estudioCategoria);
 
-    String provincia = estadistico.provinciaConMasHechosReportadosDeUnaCategoria("Incendio",fecha);
+    String provincia = estadistico.provinciaConMasHechosReportadosDeUnaCategoria("Incendio", fecha);
 
-    Assertions.assertEquals("LA_PAMPA",provincia);
+    Assertions.assertEquals("LA_PAMPA", provincia);
 
   }
 
