ff36a78932f188022bbbc606cd26558b029dedb7
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-10-27 09:59:09 -0300

    corrigo algunos errores

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -4,3 +4,12 @@ function openModal(id) {
 function closeModal(id) {
   document.getElementById(id).classList.remove('active')
 }
+function extractID(element) {
+  const modal = element.closest('.modal'); // busca el ancestro con clase "modal"
+  return modal ? modal.id : null;
+}
+function switchModal(element, nextModalID) {
+  const currentID = extractID(element);
+  if (currentID) closeModal(element);
+  openModal(nextModalID);
+}
