057e43fb11be91d0aa4e5ac2a2cdf3713c32a8b9
PGabarini <pagabarini@frba.utn.edu.ar>
2025-11-04 20:15:51 -0300

    Funcion js para crear modal de modificar coleccion dinamicamente

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -9,3 +9,15 @@ function switchModal(currentModalID, nextModalID) {
   if (currentModalID) closeModal(currentModalID);
   openModal(nextModalID);
 }
+
+function crearModalColeccion(boton) {
+  openModal('modificarColeccion')
+  const modal = document.getElementById('modificarColeccion')
+
+  const titulo = boton.dataset.titulo;
+  const descripcion = boton.dataset.descripcion;
+
+  modal.querySelector('.titulo').textContent = titulo
+  modal.querySelector('.descripcion').textContent = descripcion
+
+}
