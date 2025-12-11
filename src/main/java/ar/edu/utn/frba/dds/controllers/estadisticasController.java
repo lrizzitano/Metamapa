69e31da471490990602cc712baee6a58f4449476
Tadeo-VC <tvazquez@frba.utn.edu.ar>
2025-12-10 22:04:32 -0300

    agrego layout para contenido estadistico

--- src/main/java/ar/edu/utn/frba/dds/controllers/estadisticasController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/estadisticasController.java
@@ -2,14 +2,7 @@ package ar.edu.utn.frba.dds.controllers;
 
 import io.javalin.http.Context;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class estadisticasController {
 
-  public static void landing(Context ctx) {
-    Map<String, Object> model = new HashMap<>();
-    model.put("contenido", false);
-    ctx.render("templates/paginas/estadisticas");
-  }
+
 }
